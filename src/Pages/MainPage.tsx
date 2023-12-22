c4f07ce3c18f063fe685894bf65d05bb42e5a154
Jae Sin Cho <1005jsc@naver.com>
2023-12-23 06:37:36 +0900

    2. 로컬 서버와 connection 성공

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,7 +1,21 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useQuery } from 'react-query';
+
 const MainPage = () => {
+  const { data } = useQuery<any>('getTodos', async () => {
+    const response = await axios.get(`http://localhost:8080/clickNum`);
+
+    return response.data;
+  });
+
   return (
     <>
-      <div>MainPage</div>
+      <div>
+        MainPage
+        <p>{data.currentNum}</p>
+        <p>{data.message}</p>
+      </div>
     </>
   );
 };
